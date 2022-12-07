a6d8b6973e626fb6c6f8a71bd42da9c572b0a38d
Kien Nguyen <kiennt2609@gmail.com>
2022-12-07 18:06:22 +0700

    Add day02 & day03

---
+++ 2022/day02/day02.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import os
+
+m1 = {
+    'A': 1,
+    'B': 2,
+    'C': 3,
+    'X': 1,
+    'Y': 2,
+    'Z': 3
+}
+
+m2 = {
+    'X': 0,
+    'Y': 3,
+    'Z': 6
+}
+
+score1 = 0
+score2 = 0
+
+with open(os.path.join(os.getcwd(), "input.txt")) as f:
+    rounds = f.read().strip().split('\n')
+    for round in rounds:
+        round = round.split()
+        # Handle part 1
+        result = m1[round[1]] - m1[round[0]]
+        if result == 0:
+            score1 += (3+m1[round[1]])  # draw
+        elif result == -1 or result == 2:
+            score1 += m1[round[1]]  # lose
+        elif result == -2 or result == 1:
+            score1 += (6+m1[round[1]])  # win
+
+        # Handle part 2
+        score2 += m2[round[1]]
+        if m2[round[1]] == 0:
+            if m1[round[0]] == 1:
+                score2 += 3
+            elif m1[round[0]] == 2:
+                score2 += 1
+            elif m1[round[0]] == 3:
+                score2 += 2
+        elif m2[round[1]] == 3:
+            score2 += m1[round[0]]
+        elif m2[round[1]] == 6:
+            if m1[round[0]] == 1:
+                score2 += 2
+            elif m1[round[0]] == 2:
+                score2 += 3
+            elif m1[round[0]] == 3:
+                score2 += 1
+    print("Part 1: ", score1)
+    print("Part 2: ", score2)
---
+++ 2022/day03/day03.py
@@ -0,0 +1,26 @@
+import os
+
+# Convert a list of character to number and return sum
+def convert(items):
+    s = 0
+    for i in items:
+        if i.isupper():
+            s += ord(i)-38
+        else:
+            s += ord(i)-96
+    return s
+
+
+s1 = s2 = 0
+
+with open(os.path.join(os.getcwd(), "input.txt")) as f:
+    ruckpacks = f.read().strip().split('\n')
+    for ruckpack in ruckpacks:
+        items = (set(ruckpack[:len(ruckpack)//2]) &
+                 set(ruckpack[len(ruckpack)//2:]))
+        s1 += convert(items)
+    print("Part 1: ", s1)
+    for i in range(0, len(ruckpacks), 3):
+        items = (set(ruckpacks[i]) & set(ruckpacks[i+1]) & set(ruckpacks[i+2]))
+        s2 += convert(items)
+    print("Part 2: ", s2)
