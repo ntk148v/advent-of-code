3c5a338f8de6aa5dfa4ac70dbe00cfd215ee972c
Kien Nguyen <kiennt2609@gmail.com>
2022-12-08 17:00:24 +0700

    Finish day05

--- 2022/day05/day05.py
+++ 2022/day05/day05.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import re
 
@@ -6,14 +7,14 @@ from aocclient.client import Client
 cli = Client(year=2022, day=5)
 cli.setup()
 
-stacks = {}
+stacks_1 = {}
 
 with open(os.path.join(os.getcwd(), "input.txt")) as f:
     raw_stacks, raw_procedure = f.read().split('\n\n')
     # Process raw input
     raw_stacks = [i for i in raw_stacks.strip().split('\n')]
     # Init stacks
-    stacks = {int(k): [] for k in raw_stacks.pop().split()}
+    stacks_1 = {int(k): [] for k in raw_stacks.pop().split()}
 
     # Process - ugly but it works!
     for r in raw_stacks:
@@ -24,19 +25,27 @@ with open(os.path.join(os.getcwd(), "input.txt")) as f:
             e = v.strip().strip(']').strip('[')
             if not e:
                 continue
-            stacks[i+1].insert(0, e)
-    print("Before: ", stacks)
+            stacks_1[i+1].insert(0, e)
+    stacks_2 = copy.deepcopy(stacks_1)
+    print("Before: ", stacks_2)
 
     procedure = [re.findall(r'\d+', i)
                  for i in raw_procedure.strip().split('\n')]
     # Follow procedure
     for r in procedure:
         # Format [nums, src, dst]
-        crates = stacks[int(r[1])][-int(r[0]):]
+        # Part 1
+        crates = stacks_1[int(r[1])][-int(r[0]):]
         crates.reverse()
-        stacks[int(r[2])] += crates
-        stacks[int(r[1])] = stacks[int(r[1])][:-int(r[0])]
-    print("After: ", stacks)
-    result1 = ''.join([v[-1] for v in stacks.values()])
+        stacks_1[int(r[2])] += crates
+        stacks_1[int(r[1])] = stacks_1[int(r[1])][:-int(r[0])]
+        # Part 2
+        stacks_2[int(r[2])] += stacks_2[int(r[1])][-int(r[0]):]
+        stacks_2[int(r[1])] = stacks_2[int(r[1])][:-int(r[0])]
+    print("After: ", stacks_2)
+    result1 = ''.join([v[-1] for v in stacks_1.values()])
     print("Part 1: ", result1)
     cli.submit_answer(1, result1)
+    result2 = ''.join([v[-1] for v in stacks_2.values()])
+    print("Part 2: ", result2)
+    cli.submit_answer(1, result2)
