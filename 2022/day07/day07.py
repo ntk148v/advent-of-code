666a337178d819e61bc616d30b2da1a22ef304e5
Kien Nguyen <kiennt2609@gmail.com>
2022-12-09 14:39:20 +0700

    It's hard

--- 2022/day07/day07.py
+++ 2022/day07/day07.py
@@ -1,3 +1,4 @@
+from collections import deque
 import os
 
 from aocclient.client import Client
@@ -7,5 +8,9 @@ cli = Client(year=2022, day=7)
 ans1 = None
 ans2 = None
 
+
 with open(os.path.join(os.getcwd(), "input.txt")) as f:
-    pass
+    lines = deque(f.read().split('\n'))
+    while lines:
+        line = lines.popleft().split()
+        print(line)
