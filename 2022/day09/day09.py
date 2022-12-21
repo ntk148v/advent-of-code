ba4aadd61f809a4117e75f9bbb3269feb52d861b
Kien Nguyen <kiennt2609@gmail.com>
2022-12-21 14:58:58 +0700

    Solve day 09 - part 2

--- 2022/day09/day09.py
+++ 2022/day09/day09.py
@@ -5,44 +5,50 @@ from aocclient.client import Client
 ans1 = None
 ans2 = None
 
+# {direction: (deltax, deltay)}
+delta = {'U': (0, 1), 'D': (0, -1), 'L': (-1, 0), 'R': (1, 0)}
+
 with open(os.path.join(os.getcwd(), "input.txt")) as f:
     motions = f.read().strip().split('\n')
-    # begin at the starting position
-    head = (0, 0)
-    tail = (0, 0)
-    seen = {(0, 0)}
+    # This is a rope with 10 knots (part 2)
+    rope = [(0, 0)] * 10
+    # Tail positions
+    tail_pos_1 = {(0, 0)}
+    tail_pos_2 = {(0, 0)}
 
     for motion in motions:
         direction, steps = motion.split()
         steps = int(steps)
 
         for _ in range(steps):
-            if direction == 'D':
-                head = head[0], head[1]+1
-            elif direction == 'U':
-                head = head[0], head[1]-1
-            elif direction == 'R':
-                head = head[0]+1, head[1]
-            elif direction == 'L':
-                head = head[0]-1, head[1]
+            hx, hy = rope[0]
+            dx, dy = delta[direction]
 
-            delta = head[0]-tail[0], head[1]-tail[1]
+            # Move the head
+            rope[0] = hx+dx, hy+dy
+            for i in range(9):
+                # consider this is head
+                hx, hy = rope[i]
+                # the one after is tail
+                tx, ty = rope[i+1]
 
-            # https://en.wikipedia.org/wiki/Euclidean_distance
-            if delta[0]**2 + delta[1]**2 > 2:
-                if delta[0] > 0:
-                    tail = tail[0]+1, tail[1]
-                elif delta[0] < 0:
-                    tail = tail[0]-1, tail[1]
-                if delta[1] > 0:
-                    tail = tail[0], tail[1]+1
-                elif delta[1] < 0:
-                    tail = tail[0], tail[1]-1
-                seen.add(tail)
+                dx, dy = hx-tx, hy-ty
+                if dx**2 + dy**2 > 2:
+                    if dx != 0:
+                        tx += 1 if dx > 0 else -1
+                    if dy != 0:
+                        ty += 1 if dy > 0 else -1
+                    rope[i+1] = tx, ty
 
-ans1 = len(seen)
+            # Keep track
+            tail_pos_1.add(rope[1])
+            tail_pos_2.add(rope[9])
+
+ans1 = len(tail_pos_1)
 print("Part 1: ", ans1)
-# print("Part 2: ", ans2)
+
+ans2 = len(tail_pos_2)
+print("Part 2: ", ans2)
 cli = Client(year=2022, day=9)
-# cli.submit_answer(1, ans1)
-# cli.submit_answer(2, ans2)
+cli.submit_answer(1, ans1)
+cli.submit_answer(2, ans2)
