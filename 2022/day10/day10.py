72d520418f198652841620f88ab1e909905ef455
Kien Nguyen <kiennt2609@gmail.com>
2023-01-06 14:06:02 +0700

    Solve day 10 - part 2

--- 2022/day10/day10.py
+++ 2022/day10/day10.py
@@ -11,14 +11,28 @@ with open(os.path.join(os.getcwd(), "input.txt")) as f:
     cycle = 1
     X = 1
     total = 0
+    crt = ''
 
     for instr in program:
         instr = instr.split()
+
+        if X <= cycle % 40 <= X+2:
+            crt += '#'
+        else:
+            crt += '.'
+
         cycle += 1  # noop
         if len(instr) == 2:  # addx V
             # calculate signal strength
             if cycle % 40 == 20:
                 total += cycle * X
+            elif cycle % 40 == 1:
+                crt += '\n'
+
+            if X <= cycle % 40 <= X+2:
+                crt += '#'
+            else:
+                crt += '.'
 
             # register X
             cycle += 1
@@ -26,9 +40,20 @@ with open(os.path.join(os.getcwd(), "input.txt")) as f:
 
         if cycle % 40 == 20:
             total += cycle * X
+        elif cycle % 40 == 1:
+            crt += '\n'
 
     ans1 = total
+    print(crt) # Don't know how to convert to string to auto submit
+    # result look like this - ELPLZGZL
+    ####.#....###..#....####..##..####.#...#
+    #....#....#..#.#.......#.#..#....#.#....
+    ###..#....#..#.#......#..#......#..#....
+    #....#....###..#.....#...#.##..#...#...#
+    #....#....#....#....#....#..#.#....#....
+    ####.####.#....####.####..###.####.####.
     print("Part 1: ", ans1)
+    ans = 'ELPLZGZL'
     print("Part 2: ", ans2)
 
 cli = Client(year=2022, day=10)
