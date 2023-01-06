f64bd085fc52192f8fcf21d8413a39ca17049ff5
Kien Nguyen <kiennt2609@gmail.com>
2023-01-06 11:50:21 +0700

    Solve day 10 - part 1

--- 2022/day10/day10.py
+++ 2022/day10/day10.py
@@ -6,6 +6,28 @@ ans1 = None
 ans2 = None
 
 with open(os.path.join(os.getcwd(), "input.txt")) as f:
+    # get list of instructions
+    program = f.read().strip().split('\n')
+    cycle = 1
+    X = 1
+    total = 0
+
+    for instr in program:
+        instr = instr.split()
+        cycle += 1  # noop
+        if len(instr) == 2:  # addx V
+            # calculate signal strength
+            if cycle % 40 == 20:
+                total += cycle * X
+
+            # register X
+            cycle += 1
+            X += int(instr[1])
+
+        if cycle % 40 == 20:
+            total += cycle * X
+
+    ans1 = total
     print("Part 1: ", ans1)
     print("Part 2: ", ans2)
 
